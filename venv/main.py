bd55449593c8df766c9fc718c1cab204b4c5410c
kartik07g <72117498+kartik07g@users.noreply.github.com>
2021-10-11 18:08:24 +0530

    Edited-Getting Registration pge input at backend

--- venv/main.py
+++ venv/main.py
@@ -1,7 +1,89 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+import urllib.parse
+from flask import request
+
+username = urllib.parse.quote_plus('kartik07g')
+password = urllib.parse.quote_plus('k@123')
+
 app = Flask(__name__)
 
-@app.route("/")
+app.secret_key = "movieflix"
+client = MongoClient("mongodb+srv://%s:%s @cluster0.sht6k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority" % (
+    username, password))
+db = client.Movieflix
+print(db)
+
+
+# client = MongoClient('localhost',27017)
+# db = client.users()
+
+
+class User:
+
+    def start_session(Self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+
+    def signup(self):
+
+        # Create the user object
+        user = {
+            "_id": uuid.uuid4().hex,
+            "name": request.form.get('name'),
+            "email": request.form.get('email'),
+            "password": request.form.get('password')
+        }
+
+        # Encrypt the password
+        user['password'] = pbkdf2_sha256.encrypt(user['password'])
+
+        # Check for existing email address
+
+        if db.user.find_one({"email": user['email']}):
+            return jsonify({"error": "EMail already in use"}), 400
+
+        # users is a collection in db where we want to save user details
+        if db.users.insert_one(user):
+            return self.start_session(user)
+
+        return jsonify({"error": "SignUp Failed"}), 400
+
+    def signout(self):
+        session.clear()
+        return redirect('/')
+
+
+@app.route("/", methods=['GET', 'POST'])
+def rej():
+    if (request.method == 'POST'):
+        if (request.form.get('reg')):
+            name = request.form.get('fname')
+            email = request.form.get('email')
+            phone = request.form.get('phone')
+            password = request.form.get('password')
+            cpass = request.form.get('cpass')
+            # if (password!=cpass):
+
+#we have return modal of password confirmation
+        if (request.form.get('login')):
+            email = request.form.get('email')
+            password = request.form.get('password')
+            print(email + password)
+            return render_template("index.html")
+
+    return render_template("rejister.html")
+
+
+def rej():
+    return render_template("rejister.html")
+
+
+@app.route("/index")
 def index():
     return render_template("index.html")
+
+
 app.run(debug=True)
